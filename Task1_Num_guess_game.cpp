50e541c1e75487768375cd1e301d6ddb0e6b3a62
Yashrajkanade <140718794+Yashrajkanade@users.noreply.github.com>
2023-08-07 17:04:23 +0530

    Add files via upload

---
+++ Task1_Num_guess_game.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+using namespace std;
+int main()
+{
+    srand(time(NULL));
+    int num = rand()%100;
+    int guess;
+    cout<<"Can you guess the number between 0-100.\n";
+    while(1)
+    {
+        cout<<"Enter the guess:";
+        cin>>guess;
+        if(num==guess)
+        {
+            cout<<"You guessed it right!!!";
+            break;
+        }
+        else if (num>guess)
+        {
+            cout<<"Guess a number larger than "<<guess<<endl;
+        }
+        else
+        {
+            cout<<"Guess a number smaller than "<<guess<<endl;;
+        }
+    }
+    return 0;
+}
